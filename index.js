770f12314bfc3b9d898c28c5dc4c3d653af626d4
Chris Nady <nady.christopher@gmail.com>
2017-09-18 21:47:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+function find(array, criteriaFunc) {
+  for (let i = 0; i < array.length; i++) {
+    if (criteriaFunc(array[i])) {
+      return array[i]
+    }
+  }
+}
+
 for (let i = 0; i < scores.length; i++) {
   if (scores[i] >= 10) {
     continue
   }
- 
+
   scores[i]++
 }
